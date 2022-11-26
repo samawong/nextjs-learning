c532f2b117750f8c8c194da88ed0dd441e739cb5
samawong <samafed@gmail.com>
2022-11-26 09:39:24 +0800

    Modify use local define

--- components/features.js
+++ components/features.js
@@ -1,13 +1,34 @@
-
+/*
 async function getFeatures(){
     const resp = await fetch(`http://localhost:3000/api/features`);
     if(!resp.ok) throw new error("something is error!");
     return resp.json();
 }
-
+*/
 export default async  function Features() {
-    const data = await getFeatures();
-    console.log(data);
+    //const data = await getFeatures();
+    const data= {
+        title:"What's different about Manager?",
+        content:"Manager provider all the functionality your team needs, whitout the complexity. Out software is tailor-made for modern digital product teams.",
+        questions:[
+            {
+                id:1,
+                title:"As a component",
+                content:"You can include it as component in your document and call it as you need. This has the benefit of keeping \
+                        the SVG code in one place.It isn't much further from this to have this SVG as a component in a separate file.Once you have your SVG as a component,\
+                        you can pass it props to alter it's appearance. You receive the props and then use them directly on your SVG."
+            },{
+                id:2,
+                title:"Including directly",
+                content:"<svg> is a standard HTML tag that can be directly used in JSX. That means if your SVG is quite short, it can be easiest to include it in place."
+            },{
+                id:3,
+                title:"Using the <img> tag",
+                content:"You can use a regular img tag and reference the SVG by URL. You need to place the image in the /public directory and reference it relative to that."
+            }
+        ]
+    }
+    
     return (
         <section id="features">
             <div className="container flex flex-col px-4 mx-auto mt-10 space-y-12 md:space-y-0 md:flex-row  dark:text-white">
